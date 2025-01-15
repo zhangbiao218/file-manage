a22a7c81198c8432b95eeafefe471009ac9ed6e5
zhangbiao0812 <1013451570@qq.com>
2025-01-15 15:03:15 +0800

    修改pom和启动类,支持springcloud

--- file-manage-boot/src/main/java/com/tiansuo/file/manage/FileManageApplication.java
+++ file-manage-boot/src/main/java/com/tiansuo/file/manage/FileManageApplication.java
@@ -2,9 +2,12 @@ package com.tiansuo.file.manage;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @MapperScan("com.tiansuo.file.manage.mapper")
 @SpringBootApplication
 public class FileManageApplication {
